fea6bcbdc7d7ea02ed30630876aad86dfa1a5a57
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2024-11-21 21:52:40 -0500

    add button and hero section

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -5,6 +5,7 @@ import { Link as LinkScroll } from "react-scroll";
 // eslint-disable-next-line react/prop-types
 const NavLink = ({ title }) => (
   <LinkScroll
+  to={title}
     className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer
     hover:text-p1 max-lg:my-4 max-lg:h5"
   >
