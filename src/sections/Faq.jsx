631b1af1e55494105815559ff1ca472708800abf
Dany.Makhoul <dany.makhoul@dawsoncollege.qc.ca>
2024-11-26 15:40:12 -0500

    add download section

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -5,7 +5,7 @@ const Faq = () => {
   const halfLength = Math.floor(faq.length / 2);
   return (
     <section>
-      <Element name="faq" classname="relative">
+      <Element name="faq" className="relative">
         <div className="container relative z-2 py-28">
           <div>
             <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
